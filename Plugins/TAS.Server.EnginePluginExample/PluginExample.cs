3923dce48167a668f4c77ed5db9ffa1d37aa2614
Jurek <jurek@tvp.pl>
2016-08-23 22:58:17 +0200

    minor update of EnginePluginExample

--- Plugins/TAS.Server.EnginePluginExample/PluginExample.cs
+++ Plugins/TAS.Server.EnginePluginExample/PluginExample.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -38,13 +39,19 @@ namespace TAS.Server.EnginePluginExample
                     parameters.Add("eventDuration", ((int)e.Event.Duration.TotalMilliseconds).ToString());
                     uri.Query = string.Join("&", parameters.Select(kv => string.Format("{0}={1}", kv.Key, kv.Value)));
                     HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri.Uri);
-                    req.Method = "POST";
-                    using (var writer = new System.IO.StreamWriter(req.GetRequestStream()))
+                    using (MemoryStream jsonData = new MemoryStream())
+                        using( var writer = new System.IO.StreamWriter(jsonData))
                     {
                         new Newtonsoft.Json.JsonSerializer() { Formatting = Newtonsoft.Json.Formatting.Indented, PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None }
                         .Serialize(writer, EventProxy.FromEvent(e.Event));
+                        req.Method = "POST";
+                        req.ContentLength = jsonData.Length;
+                        req.ContentType = "application/json";
+                        Stream reqStream = req.GetRequestStream();
+                        jsonData.WriteTo(reqStream);
+                        reqStream.Close();
                     }
-                    req.BeginGetResponse(null, null);
+                    req.GetResponse();
                 }
             }
         }
