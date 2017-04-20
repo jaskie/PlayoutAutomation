cc39b769c30ec7988c09bb2d8397b02bed390be0
Jerzy Jaśkiewicz <jurek@tvp.pl>
2017-04-20 16:08:18 +0200

    added record toggle button with animation

--- TAS.Client/ViewModels/RecordersViewmodel.cs
+++ TAS.Client/ViewModels/RecordersViewmodel.cs
@@ -28,6 +28,238 @@ namespace TAS.Client.ViewModels
             Recorder = _recorders.FirstOrDefault();
         }
 
+
+        public ICommand CommandPlay { get; private set; }
+        public ICommand CommandStop { get; private set; }
+        public ICommand CommandFastForward { get; private set; }
+        public ICommand CommandRewind { get; private set; }
+        public ICommand CommandAbort { get; private set; }
+        public ICommand CommandCapture { get; private set; }
+        public ICommand CommandGetCurrentTcToIn { get; private set; }
+        public ICommand CommandGetCurrentTcToOut { get; private set; }
+        public ICommand CommandGoToTimecode { get; private set; }
+        public ICommand CommandRecordEnd { get; private set; }
+        public ICommand CommandRecordFinish { get; private set; }
+        public ICommand CommandSetRecordLimit { get; private set; }
+
+        public bool IsRecording
+        {
+            get { return _recordMedia?.MediaStatus == TMediaStatus.Copying; }
+            set
+            {
+                if (value)
+                    _startRecord();
+                else
+                    _finishRecord(null);
+            }
+        }
+
+        public bool CanStartRecord
+        {
+            get { return _recorder != null && _channel != null && Recorder.IsServerConnected && string.IsNullOrEmpty(_validateFileName()); }
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set
+            {
+                if (SetField(ref _fileName, value, nameof(FileName)))
+                    NotifyPropertyChanged(nameof(CanStartRecord));
+            }
+        }
+
+        public IRecorder Recorder
+        {
+            get { return _recorder; }
+            set
+            {
+                var oldRecorder = _recorder;
+                if (SetField(ref _recorder, value, nameof(Recorder)))
+                {
+                    if (oldRecorder != null)
+                        oldRecorder.PropertyChanged -= Recorder_PropertyChanged;
+                    if (value != null)
+                        value.PropertyChanged += Recorder_PropertyChanged;
+                    Channels = value.Channels;
+                    Channel = value.Channels.LastOrDefault();
+                }
+            }
+        }
+
+        public IEnumerable<IRecorder> Recorders { get { return _recorders; } }
+
+        public IEnumerable<IPlayoutServerChannel> Channels { get { return _channels; } private set { SetField(ref _channels, value, nameof(Channels)); } }
+
+        public IPlayoutServerChannel Channel
+        {
+            get { return _channel; }
+            set
+            {
+                if (SetField(ref _channel, value, nameof(Channel)))
+                {
+                    VideoFormat = value.VideoFormat;
+                    NotifyPropertyChanged(nameof(CanStartRecord));
+                }
+            }
+        }
+
+        public Array FileFormats { get { return Enum.GetValues(typeof(TMovieContainerFormat)); } }
+
+        private TMovieContainerFormat _fileFormat;
+        public TMovieContainerFormat FileFormat { get { return _fileFormat; } set { SetField(ref _fileFormat, value, nameof(FileFormat)); } }
+
+        public bool IsNarrowMode { get { return _isNarrowMode; } set { SetField(ref _isNarrowMode, value, nameof(IsNarrowMode)); } }
+
+        public TimeSpan TcIn { get { return _tcIn; } set { SetField(ref _tcIn, value, nameof(TcIn)); } }
+
+        public TimeSpan TcOut { get { return _tcOut; } set { SetField(ref _tcOut, value, nameof(TcOut)); } }
+
+        public TimeSpan CurrentTc { get { return _currentTc; }  set { SetField(ref _currentTc, value, nameof(CurrentTc)); } }
+
+        public TimeSpan TimeLimit
+        {
+            get { return _timeLimit; }
+            set { SetField(ref _timeLimit, value, nameof(TimeLimit)); }
+        }
+
+        public TimeSpan RecorderTimeLeft
+        {
+            get { return _recorderTimeLeft; }
+            private set { SetField(ref _recorderTimeLeft, value, nameof(RecorderTimeLeft)); }
+        }
+
+        public TDeckState DeckState
+        {
+            get { return _deckState; }
+            private set
+            {
+                if (SetField(ref _deckState, value, nameof(DeckState)))
+                    InvalidateRequerySuggested();
+            }
+        }
+
+        public TDeckControl DeckControl
+        {
+            get { return _deckControl; }
+            private set
+            {
+                if (SetField(ref _deckControl, value, nameof(DeckControl)))
+                    InvalidateRequerySuggested();
+            }
+        }
+
+        public TVideoFormat VideoFormat
+        {
+            get { return _videoFormat; }
+            private set
+            {
+                if (SetField(ref _videoFormat, value, nameof(VideoFormat)))
+                    NotifyPropertyChanged(nameof(IsNarrowMode));
+            }
+        }
+
+        public IMedia RecordMedia
+        {
+            get { return _recordMedia; }
+            private set
+            {
+                var oldRecordMedia = _recordMedia;
+                if (SetField(ref _recordMedia, value, nameof(RecordMedia)))
+                {
+                    if (oldRecordMedia != null)
+                        oldRecordMedia.PropertyChanged -= RecordMedia_PropertyChanged;
+                    if (value != null)
+                        value.PropertyChanged += RecordMedia_PropertyChanged;
+                }
+            }
+        }
+
+        
+        public bool IsStandardDefinition { get { return _videoFormat < TVideoFormat.HD720p2500; } }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                string validationResult = null;
+                switch (propertyName)
+                {
+                    case nameof(FileName):
+                    case nameof(FileFormat):
+                        validationResult = _validateFileName();
+                        break;
+                }
+                return validationResult;
+            }
+        }
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        protected override void OnDispose()
+        {
+            Recorder = null; // ensure that events are disconnected
+            RecordMedia = null;
+        }
+
+        private void Recorder_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (Application.Current == null)
+                return;
+            if (e.PropertyName == nameof(IRecorder.CurrentTc))
+                Application.Current.Dispatcher.BeginInvoke((Action)(() => CurrentTc = ((IRecorder)sender).CurrentTc));
+            if (e.PropertyName == nameof(IRecorder.DeckControl))
+                Application.Current.Dispatcher.BeginInvoke((Action)(() => DeckControl = ((IRecorder)sender).DeckControl));
+            if (e.PropertyName == nameof(IRecorder.DeckState))
+                Application.Current.Dispatcher.BeginInvoke((Action)(() => DeckState = ((IRecorder)sender).DeckState));
+            if (e.PropertyName == nameof(IRecorder.IsDeckConnected)
+                || e.PropertyName == nameof(IRecorder.IsServerConnected))
+                NotifyPropertyChanged(null);
+            if (e.PropertyName == nameof(IRecorder.TimeLimit))
+                Application.Current.Dispatcher.BeginInvoke((Action)(() => RecorderTimeLeft = ((IRecorder)sender).TimeLimit));
+        }
+
+        private void RecordMedia_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(IMedia.MediaStatus))
+            {
+                NotifyPropertyChanged(nameof(IsRecording));
+                NotifyPropertyChanged(nameof(CanStartRecord));
+            }
+        }
+
+        private string _validateFileName()
+        {
+            if (string.IsNullOrWhiteSpace(_fileName))
+                return resources._validate_FileNameEmpty;
+            string newName = $"{_fileName}.{_fileFormat}";
+            if (newName.StartsWith(" ") || newName.EndsWith(" "))
+                return resources._validate_FileNameCanNotStartOrEndWithSpace;
+            if (newName.IndexOfAny(Path.GetInvalidFileNameChars()) > 0)
+                return resources._validate_FileNameCanNotContainSpecialCharacters;
+            if (_recorder?.RecordingDirectory.FileExists(newName) == true)
+                return resources._validate_FileAlreadyExists;
+            return string.Empty;
+        }
+
+        private string _fileName;
+        private IRecorder _recorder;
+        private IEnumerable<IPlayoutServerChannel> _channels;
+        private IPlayoutServerChannel _channel;
+        private TimeSpan _tcIn;
+        private bool _isNarrowMode;
+        private TimeSpan _tcOut;
+        private TimeSpan _currentTc;
+        private TimeSpan _timeLimit;
+        private IMedia _recordMedia;
+        private TVideoFormat _videoFormat;
+        private TDeckControl _deckControl;
+        private TDeckState _deckState;
+        private TimeSpan _recorderTimeLeft;
+
         private void _createCommands()
         {
             CommandAbort = new UICommand { ExecuteDelegate = _abort, CanExecuteDelegate = _canAbort };
@@ -40,7 +272,6 @@ namespace TAS.Client.ViewModels
             CommandGetCurrentTcToOut = new UICommand { ExecuteDelegate = o => TcOut = CurrentTc };
             CommandGoToTimecode = new UICommand { ExecuteDelegate = _goToTimecode, CanExecuteDelegate = _canGoToTimecode };
             CommandSetRecordLimit = new UICommand { ExecuteDelegate = _setRecordTimeLimit, CanExecuteDelegate = _canSetRecordTimeLimit };
-            CommandRecordStart = new UICommand { ExecuteDelegate = _startRecord, CanExecuteDelegate = _canStartRecord };
             CommandRecordFinish = new UICommand { ExecuteDelegate = _finishRecord, CanExecuteDelegate = _canFinishRecord };
         }
 
@@ -54,12 +285,7 @@ namespace TAS.Client.ViewModels
             _recorder.Finish();
         }
 
-        private bool _canStartRecord(object obj)
-        {
-            return _recorder != null && _channel != null && Recorder.IsServerConnected && string.IsNullOrEmpty(_validateFileName());
-        }
-
-        private void _startRecord(object obj)
+        private void _startRecord()
         {
             RecordMedia = _recorder?.Capture(_channel, _timeLimit, IsNarrowMode, $"{FileName}.{FileFormat}");
         }
@@ -136,180 +362,5 @@ namespace TAS.Client.ViewModels
             throw new NotImplementedException();
         }
 
-        public ICommand CommandPlay { get; private set; }
-        public ICommand CommandStop { get; private set; }
-        public ICommand CommandFastForward { get; private set; }
-        public ICommand CommandRewind { get; private set; }
-        public ICommand CommandAbort { get; private set; }
-        public ICommand CommandCapture { get; private set; }
-        public ICommand CommandGetCurrentTcToIn { get; private set; }
-        public ICommand CommandGetCurrentTcToOut { get; private set; }
-        public ICommand CommandGoToTimecode { get; private set; }
-        public ICommand CommandRecordEnd { get; private set; }
-        public ICommand CommandRecordStart { get; private set; }
-        public ICommand CommandRecordFinish { get; private set; }
-        public ICommand CommandSetRecordLimit { get; private set; }
-
-        private string _fileName;
-        public string FileName { get { return _fileName; } set { SetField(ref _fileName, value, nameof(FileName)); } }
-
-        private IRecorder _recorder;
-        public IRecorder Recorder
-        {
-            get { return _recorder; }
-            set
-            {
-                var oldRecorder = _recorder;
-                if (SetField(ref _recorder, value, nameof(Recorder)))
-                {
-                    if (oldRecorder != null)
-                        oldRecorder.PropertyChanged -= _recorder_PropertyChanged;
-                    if (value != null)
-                        value.PropertyChanged += _recorder_PropertyChanged;
-                    Channels = value.Channels;
-                    Channel = value.Channels.LastOrDefault();
-                }
-            }
-        }
-
-        public IEnumerable<IRecorder> Recorders { get { return _recorders; } }
-
-        private IEnumerable<IPlayoutServerChannel> _channels;
-        public IEnumerable<IPlayoutServerChannel> Channels { get { return _channels; } private set { SetField(ref _channels, value, nameof(Channels)); } }
-
-        private IPlayoutServerChannel _channel;
-        public IPlayoutServerChannel Channel
-        {
-            get { return _channel; }
-            set
-            {
-                if (SetField(ref _channel, value, nameof(Channel)))
-                    VideoFormat = value.VideoFormat;
-            }
-        }
-
-        public Array FileFormats { get { return Enum.GetValues(typeof(TMovieContainerFormat)); } }
-
-        private TMovieContainerFormat _fileFormat;
-        public TMovieContainerFormat FileFormat { get { return _fileFormat; } set { SetField(ref _fileFormat, value, nameof(FileFormat)); } }
-
-        private bool _isNarrowMode;
-        public bool IsNarrowMode { get { return _isNarrowMode; } set { SetField(ref _isNarrowMode, value, nameof(IsNarrowMode)); } }
-
-        private TimeSpan _tcIn;
-        public TimeSpan TcIn { get { return _tcIn; } set { SetField(ref _tcIn, value, nameof(TcIn)); } }
-
-        private TimeSpan _tcOut;
-        public TimeSpan TcOut { get { return _tcOut; } set { SetField(ref _tcOut, value, nameof(TcOut)); } }
-
-        private TimeSpan _currentTc;
-        public TimeSpan CurrentTc { get { return _currentTc; }  set { SetField(ref _currentTc, value, nameof(CurrentTc)); } }
-
-        private TimeSpan _timeLimit;
-        public TimeSpan TimeLimit
-        {
-            get { return _timeLimit; }
-            set { SetField(ref _timeLimit, value, nameof(TimeLimit)); }
-        }
-
-        private TimeSpan _recorderTimeLeft;
-        public TimeSpan RecorderTimeLeft
-        {
-            get { return _recorderTimeLeft; }
-            private set { SetField(ref _recorderTimeLeft, value, nameof(RecorderTimeLeft)); }
-        }
-
-        private TDeckState _deckState;
-        public TDeckState DeckState
-        {
-            get { return _deckState; }
-            private set
-            {
-                if (SetField(ref _deckState, value, nameof(DeckState)))
-                    InvalidateRequerySuggested();
-            }
-        }
-
-        private TDeckControl _deckControl;
-        public TDeckControl DeckControl
-        {
-            get { return _deckControl; }
-            private set
-            {
-                if (SetField(ref _deckControl, value, nameof(DeckControl)))
-                    InvalidateRequerySuggested();
-            }
-        }
-
-        private TVideoFormat _videoFormat;
-        public TVideoFormat VideoFormat
-        {
-            get { return _videoFormat; }
-            private set
-            {
-                if (SetField(ref _videoFormat, value, nameof(VideoFormat)))
-                    NotifyPropertyChanged(nameof(IsNarrowMode));
-            }
-        }
-
-        private IMedia _recordMedia;
-        public IMedia RecordMedia { get { return _recordMedia; } private set { SetField(ref _recordMedia, value, nameof(RecordMedia)); } }
-
-        public bool IsStandardDefinition { get { return _videoFormat < TVideoFormat.HD720p2500; } }
-
-        public string this[string propertyName]
-        {
-            get
-            {
-                string validationResult = null;
-                switch (propertyName)
-                {
-                    case nameof(FileName):
-                    case nameof(FileFormat):
-                        validationResult = _validateFileName();
-                        break;
-                }
-                return validationResult;
-            }
-        }
-
-        private string _validateFileName()
-        {
-            if (string.IsNullOrWhiteSpace(_fileName))
-                return resources._validate_FileNameEmpty;
-            string newName = $"{_fileName}.{_fileFormat}";
-            if (newName.StartsWith(" ") || newName.EndsWith(" "))
-                return resources._validate_FileNameCanNotStartOrEndWithSpace;
-            if (newName.IndexOfAny(Path.GetInvalidFileNameChars()) > 0)
-                return resources._validate_FileNameCanNotContainSpecialCharacters;
-            if (_recorder?.RecordingDirectory.FileExists(newName) == true)
-                    return resources._validate_FileAlreadyExists;
-            return string.Empty;
-        }
-
-        public string Error
-        {
-            get { return null; }
-        }
-
-        private void _recorder_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(IRecorder.CurrentTc))
-                Application.Current.Dispatcher.BeginInvoke((Action)(() => CurrentTc = ((IRecorder)sender).CurrentTc));
-            if (e.PropertyName == nameof(IRecorder.DeckControl))
-                Application.Current.Dispatcher.BeginInvoke((Action)(() => DeckControl = ((IRecorder)sender).DeckControl));
-            if (e.PropertyName == nameof(IRecorder.DeckState))
-                Application.Current.Dispatcher.BeginInvoke((Action)(() => DeckState = ((IRecorder)sender).DeckState));
-            if (e.PropertyName == nameof(IRecorder.IsDeckConnected)
-                || e.PropertyName == nameof(IRecorder.IsServerConnected))
-                NotifyPropertyChanged(null);
-            if (e.PropertyName == nameof(IRecorder.TimeLimit))
-                Application.Current.Dispatcher.BeginInvoke((Action)(() => RecorderTimeLeft = ((IRecorder)sender).TimeLimit));
-        }
-
-        protected override void OnDispose()
-        {
-            Recorder = null; // ensure that events are disconnected
-        }
     }
 }
