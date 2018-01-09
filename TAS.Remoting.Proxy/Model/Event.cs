9540241e0e1af41f1bd9ea54b592d6c51f47ed39
Jerzy Jaśkiewicz <jurek@tvp.pl>
2018-01-09 16:19:17 +0100

    Fixed remote client container content display

--- TAS.Remoting.Proxy/Model/Event.cs
+++ TAS.Remoting.Proxy/Model/Event.cs
@@ -135,6 +135,7 @@ namespace TAS.Remoting.Model
 
         private Lazy<IEvent> _prior;
 
+        [JsonProperty(nameof(IEvent.SubEvents))]
         private Lazy<IEnumerable<IEvent>> _subEvents;
 
         #pragma warning restore
