dd28419da6405ddca23a76432726e5dac16ad907
Jerzy Jaśkiewicz <Jurek.Jaskiewicz@gmail.com>
2018-08-07 16:35:07 +0200

    started v1.4

--- TAS.Client.Common/Properties/AssemblyInfo.cs
+++ TAS.Client.Common/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TVPlay - Open play-out automation")]
-[assembly: AssemblyCopyright("2017")]
+[assembly: AssemblyCopyright("2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,8 +33,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
