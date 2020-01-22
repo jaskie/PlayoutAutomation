91b5c9bbd27e32930b91c05b599d3a3d3ec30782
Michal Jaskiewicz <mj@zcui.pl>
2020-01-22 14:30:47 +0100

    IDatabase implementation for SQLite

---
+++ DatabaseProviders/SQLite/DbEventExtensions.cs
@@ -0,0 +1,30 @@
+﻿using TAS.Common;
+using TAS.Common.Interfaces;
+
+namespace TAS.Database.SQLite
+{
+    public static class DbEventExtensions
+    {
+        public static uint ToFlags(this IEvent ev)
+        {
+            return (uint)((ev.IsEnabled ? 1 : 0) << 0
+             | (ev.IsHold ? 1 : 0) << 1
+             | (ev.IsLoop ? 1 : 0) << 2
+             | (ev.IsCGEnabled ? 1 : 0) << 4
+             | (ev.Parental & 0xF) << 6
+             | (ev.Logo & 0xF) << 10
+             | (ev.Crawl & 0xF) << 14
+             | ((byte)ev.AutoStartFlags) << 20)
+             ;
+        }
+
+        public static bool IsEnabled(this uint flags) { return (flags & (1 << 0)) != 0; }
+        public static bool IsHold(this uint flags) { return (flags & (1 << 1)) != 0; }
+        public static bool IsLoop(this uint flags) { return (flags & (1 << 2)) != 0; }
+        public static bool IsCGEnabled(this uint flags) { return (flags & (1 << 4)) != 0; }
+        public static byte Parental (this uint flags) { return (byte)((flags >> 6) & 0xF); }
+        public static byte Logo(this uint flags) { return (byte)((flags >> 10) & 0xF); }
+        public static byte Crawl(this uint flags) { return (byte)((flags >> 14) & 0xF); }
+        public static AutoStartFlags AutoStartFlags(this uint flags) { return (AutoStartFlags)((flags >> 20) & 0x0F); }
+    }
+}
---
+++ DatabaseProviders/SQLite/SQLiteDataReaderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAS.Database.SQLite
+{
+    public static class SQLiteDataReaderExtensions
+    {
+        public static Int32 GetInt32(this SQLiteDataReader dataReader, string name)
+        {
+            return dataReader.GetInt32(dataReader.GetOrdinal(name));
+        }
+        public static uint GetUInt32(this SQLiteDataReader dataReader, string name)
+        {
+            return (uint)dataReader.GetInt32(dataReader.GetOrdinal(name));
+        }
+        public static bool IsDBNull(this SQLiteDataReader dataReader, string name)
+        {
+            return dataReader.IsDBNull(dataReader.GetOrdinal(name));
+        }
+        public static DateTime GetDateTime(this SQLiteDataReader dataReader, string name)
+        {
+            var index = dataReader.GetOrdinal(name);
+            if (dataReader.IsDBNull(index))
+                return new DateTime();
+            else
+                return new DateTime(dataReader.GetDateTime(index).Ticks);
+        }
+        public static Guid GetGuid(this SQLiteDataReader dataReader, string name)
+        {
+            var index = dataReader.GetOrdinal(name);
+            return dataReader.IsDBNull(index) ? new Guid() : dataReader.GetGuid(index);
+        }
+        public static long GetInt64(this SQLiteDataReader dataReader, string name)
+        {
+            return (long)dataReader.GetInt64(dataReader.GetOrdinal(name));
+        }
+        public static UInt64 GetUInt64(this SQLiteDataReader dataReader, string name)
+        {
+            var index = dataReader.GetOrdinal(name);
+            return dataReader.IsDBNull(index) ? 0UL : (ulong)dataReader.GetInt64(index);
+        }
+        public static string GetString(this SQLiteDataReader dataReader, string name)
+        {
+            var index = dataReader.GetOrdinal(name);
+            return dataReader.IsDBNull(index) ? "" : dataReader.GetString(index);
+        }
+        public static byte GetByte(this SQLiteDataReader dataReader, string name)
+        {
+            return dataReader.GetByte(dataReader.GetOrdinal(name));
+        }
+        public static double GetDouble(this SQLiteDataReader dataReader, string name)
+        {
+            return dataReader.GetDouble(dataReader.GetOrdinal(name));
+        }
+        public static short GetInt16(this SQLiteDataReader dataReader, string name)
+        {
+            return dataReader.GetInt16(dataReader.GetOrdinal(name));
+        }
+        public static TimeSpan GetTimeSpan(this SQLiteDataReader dataReader, string name)
+        { 
+            var index = dataReader.GetOrdinal(name);
+            return dataReader.IsDBNull(index) ? new TimeSpan() : new TimeSpan((long)dataReader.GetInt64(index));
+        }
+    }
+}
