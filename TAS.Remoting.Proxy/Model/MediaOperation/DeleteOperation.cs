4bf5f3c055d909e52a11497c4c960ec40ca1204a
Jerzy Jaśkiewicz <jurek.jaskiewicz@gmail.com>
2025-01-28 20:00:46 +0100

    Updated jNet.RPC. Added server+client launch configuration, removed extra code from DeleteOperation

--- TAS.Remoting.Proxy/Model/MediaOperation/DeleteOperation.cs
+++ TAS.Remoting.Proxy/Model/MediaOperation/DeleteOperation.cs
@@ -11,12 +11,6 @@ namespace TAS.Remoting.Model.MediaOperation
     {
         #pragma warning disable CS0649, CS0169
 
-        [DtoMember(nameof(ILoudnessOperation.MeasureDuration))]
-        private TimeSpan _measureDuration;
-
-        [DtoMember(nameof(ILoudnessOperation.MeasureStart))]
-        private TimeSpan _measureStart;
-
         [DtoMember(nameof(ILoudnessOperation.Source))]
         private MediaBase _source;
 
@@ -26,31 +20,5 @@ namespace TAS.Remoting.Model.MediaOperation
 
         public IMedia Source { get => _source; set => Set(value); }
 
-        public TimeSpan MeasureDuration { get => _measureDuration; set => Set(value); }
-
-        public TimeSpan MeasureStart { get => _measureStart; set => Set(value); }
-
-        public event EventHandler<AudioVolumeEventArgs> AudioVolumeMeasured
-        {
-            add
-            {
-                EventAdd(_audioVolumeMeasured);
-                _audioVolumeMeasured += value;
-            }
-
-            remove
-            {
-                _audioVolumeMeasured -= value;
-                EventRemove(_audioVolumeMeasured);
-            }
-        }
-
-        protected override void OnEventNotification(string eventName, EventArgs eventArgs)
-        {
-            if (eventName == nameof(AudioVolumeMeasured))
-                _audioVolumeMeasured?.Invoke(this, (AudioVolumeEventArgs)eventArgs);
-            else
-                base.OnEventNotification(eventName, eventArgs);
-        }
     }
 }
