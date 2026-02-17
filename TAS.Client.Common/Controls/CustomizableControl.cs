61e8682850a1465535b67f123c172dc980c601cd
Jerzy Jaśkiewicz <jurek.jaskiewicz@gmail.com>
2026-02-17 22:12:22 +0100

    Added support for dependency and attached properties. Added and commented out the examples.

--- TAS.Client.Common/Controls/CustomizableControl.cs
+++ TAS.Client.Common/Controls/CustomizableControl.cs
@@ -24,28 +24,31 @@ namespace TAS.Client.Common.Controls
 
         public string ViewName
         {
-            get => (string) GetValue(ViewNameProperty);
+            get => (string)GetValue(ViewNameProperty);
             set => SetValue(ViewNameProperty, value);
         }
     }
 
     internal class CustomizableControlPropertyStore
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger(); 
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private CustomizableControlPropertyStore() { }
 
         private Dictionary<string, IList<CustomizedPropertySetter>> _customizedProperties = new Dictionary<string, IList<CustomizedPropertySetter>>();
 
         public static CustomizableControlPropertyStore Current { get; } = new CustomizableControlPropertyStore();
+
         public void SetControlCustomizedProperties(CustomizableControl view)
         {
             if (string.IsNullOrWhiteSpace(view.ViewName))
                 return;
+
             if (!_customizedProperties.TryGetValue(view.ViewName, out var properties))
             {
                 properties = LoadConfiguration(view);
                 _customizedProperties.Add(view.ViewName, properties);
             }
+
             ApplyProperties(view, properties);
         }
 
@@ -57,13 +60,12 @@ namespace TAS.Client.Common.Controls
 
         private IList<CustomizedPropertySetter> LoadConfiguration(CustomizableControl view)
         {
-
             var fileName = Path.Combine(FileUtils.ConfigurationPath, "UI", $"{view.ViewName}.xml");
             if (File.Exists(fileName))
             {
                 try
                 {
-                    XDocument configuration = XDocument.Load(fileName);
+                    var configuration = XDocument.Load(fileName);
                     var setters = new List<CustomizedPropertySetter>();
                     foreach (var element in configuration.Root.Elements())
                     {
@@ -88,8 +90,17 @@ namespace TAS.Client.Common.Controls
 
         private class CustomizedPropertySetter
         {
+            private static readonly IDictionary<string, Type> AttachedOwnerAliases = new Dictionary<string, Type>(StringComparer.Ordinal)
+            {
+                { "Grid", typeof(Grid) },
+                { "DockPanel", typeof(DockPanel) },
+                { "Canvas", typeof(Canvas) },
+                { "Panel", typeof(Panel) }
+            };
+
             private readonly string _controlName;
-            private readonly PropertyInfo _property;
+            private readonly DependencyProperty _dependencyProperty;
+            private readonly PropertyInfo _propertyInfo;
             private readonly object _value;
 
             public static bool TryAdd(CustomizableControl view, string controlName, string propertyName, string value, out CustomizedPropertySetter propertySetter)
@@ -101,34 +112,159 @@ namespace TAS.Client.Common.Controls
                     Logger.Error("Unable to add customization for view {0} - control with name {1} not found", view.ViewName, controlName);
                     return false;
                 }
+
+                // Regular CLR property
+                if (TryCreateClrPropertySetter(control, view.ViewName, controlName, propertyName, value, out propertySetter))
+                    return true;
+
+                // Regular DP on the target type (e.g. WidthProperty)
+                if (TryCreateDependencyPropertySetter(control, view.ViewName, controlName, propertyName, value, out propertySetter))
+                    return true;
+
+                // Attached property: "Grid.Row", "DockPanel.Dock", etc.
+                if (TryCreateAttachedPropertySetter(view.ViewName, controlName, propertyName, value, out propertySetter))
+                    return true;
+
+                Logger.Error("Unable to add customization for control {0} on view {1}, property {2} - property not identified", controlName, view.ViewName, propertyName);
+                return false;
+            }
+
+            private static bool TryCreateClrPropertySetter(object control, string viewName, string controlName, string propertyName, string value, out CustomizedPropertySetter setter)
+            {
                 var pi = control.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-                if (pi is null)
+                setter = null;
+                if (pi != null)
                 {
-                    Logger.Error("Unable to add customization for control {0} on view {1}, property {2} - property does not exists", controlName, view.ViewName, propertyName);
+                    if (!pi.CanWrite)
+                    {
+                        Logger.Error("Unable to add customization for control {0} on view {1}, property {2} - property is not writable", controlName, viewName, propertyName);
+                        return false;
+                    }
+                    try
+                    {
+                        var convertedValue = ConvertFromString(value, pi.PropertyType);
+                        setter = new CustomizedPropertySetter(controlName, pi, convertedValue);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Error(e, "Unable to add customization for control {0} on view {1}, property {2} - unable to convert value {3} to expected type {4}",
+                            controlName, viewName, propertyName, value, pi.PropertyType);
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+            private static bool TryCreateDependencyPropertySetter(object control, string viewName, string controlName, string propertyName, string value, out CustomizedPropertySetter setter)
+            {
+                setter = null;
+
+                var dp = FindDependencyProperty(control.GetType(), propertyName);
+                if (dp == null)
+                    return false;
+
+                if (dp.ReadOnly)
+                {
+                    Logger.Error("Unable to add customization for control {0} on view {1}, dependency property {2} - dependency property is read-only",
+                        controlName, viewName, propertyName);
                     return false;
                 }
-                if (!pi.CanWrite)
-                {
-                    Logger.Error("Unable to add customization for control {0} on view {1}, property {2} - property is not writable", controlName, view.ViewName, propertyName);
-                    return false;
-                }
+
                 try
                 {
-                    var convertedValue = ConvertFromString(value, pi.PropertyType);
-                    propertySetter = new CustomizedPropertySetter(controlName, pi, convertedValue);
+                    var convertedValue = ConvertFromString(value, dp.PropertyType);
+                    setter = new CustomizedPropertySetter(controlName, dp, convertedValue);
                     return true;
                 }
                 catch (Exception e)
                 {
-                    Logger.Error(e, "Unable to add customization for control {0} on view {1}, property {2} - unable to convert value {3} to expected type {4}", controlName, view.ViewName, propertyName, value, pi.PropertyType);
+                    Logger.Error(e, "Unable to add customization for control {0} on view {1}, dependency property {2} - unable to convert value {3} to expected type {4}",
+                        controlName, viewName, propertyName, value, dp.PropertyType);
                     return false;
                 }
             }
 
+            private static bool TryCreateAttachedPropertySetter(string viewName, string controlName, string propertyName, string value, out CustomizedPropertySetter setter)
+            {
+                setter = null;
+
+                var dot = propertyName.IndexOf('.');
+                if (dot <= 0 || dot >= propertyName.Length - 1)
+                    return false;
+
+                var ownerName = propertyName.Substring(0, dot);
+                var attachedName = propertyName.Substring(dot + 1);
+
+                if (!TryResolveOwnerType(ownerName, out var ownerType))
+                {
+                    Logger.Error("Unable to add customization for control {0} on view {1}, attached property {2} - owner type not resolved", controlName, viewName, propertyName);
+                    return false;
+                }
+
+                var dp = FindDependencyProperty(ownerType, attachedName);
+                if (dp == null)
+                {
+                    Logger.Error("Unable to add customization for control {0} on view {1}, attached property {2} - property not found", controlName, viewName, propertyName);
+                    return false;
+                }
+
+                if (dp.ReadOnly)
+                {
+                    Logger.Error("Unable to add customization for control {0} on view {1}, attached property {2} - dependency property is read-only", controlName, viewName, propertyName);
+                    return false;
+                }
+
+                try
+                {
+                    var convertedValue = ConvertFromString(value, dp.PropertyType);
+                    setter = new CustomizedPropertySetter(controlName, dp, convertedValue);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e, "Unable to add customization for control {0} on view {1}, attached property {2} - unable to convert value {3} to expected type {4}",
+                        controlName, viewName, propertyName, value, dp.PropertyType);
+                }
+                return false;
+            }
+
+            private static bool TryResolveOwnerType(string ownerName, out Type ownerType)
+            {
+                ownerType = null;
+
+                if (AttachedOwnerAliases.TryGetValue(ownerName, out ownerType))
+                    return true;
+
+                ownerType = Type.GetType(ownerName, throwOnError: false);
+                return ownerType != null;
+            }
+
+            private static DependencyProperty FindDependencyProperty(Type ownerType, string propertyName)
+            {
+                var dpField = ownerType.GetField(propertyName + "Property",
+                    BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+
+                return dpField?.GetValue(null) as DependencyProperty;
+            }
+
             private CustomizedPropertySetter(string controlName, PropertyInfo property, object value)
             {
+                if (property == null)
+                    throw new ArgumentNullException(nameof(property));
+
                 _controlName = controlName;
-                _property = property;
+                _propertyInfo = property;
+                _value = value;
+            }
+
+            private CustomizedPropertySetter(string controlName, DependencyProperty property, object value)
+            {
+                if (property == null)
+                    throw new ArgumentNullException(nameof(property));
+
+                _controlName = controlName;
+                _dependencyProperty = property;
                 _value = value;
             }
 
@@ -137,12 +273,20 @@ namespace TAS.Client.Common.Controls
                 var control = contentControl.FindName(_controlName);
                 if (control is null)
                 {
-                    Logger.Error("Unable to find control of name {0} of view {1} to set its property {2}", _controlName, viewName, _property.Name);
+                    Logger.Error("Unable to find control of name {0} of view {1} to set its property", _controlName, viewName);
                     return;
                 }
+
                 try
                 {
-                    _property.SetValue(control, _value);
+                    if (_propertyInfo != null)
+                    {
+                        _propertyInfo.SetValue(control, _value);
+                        return;
+                    }
+
+                    if (_dependencyProperty != null && control is DependencyObject dependencyObject)
+                        dependencyObject.SetValue(_dependencyProperty, _value);
                 }
                 catch (Exception e)
                 {
@@ -159,6 +303,5 @@ namespace TAS.Client.Common.Controls
                 return Convert.ChangeType(value, targetType, CultureInfo.InvariantCulture);
             }
         }
-
     }
 }
